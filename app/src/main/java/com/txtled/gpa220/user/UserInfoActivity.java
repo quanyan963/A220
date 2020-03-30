2c446284e8ace5974c5ebb21d23770782d0f7bd2
quanyan963 <709641706@qq.com>
2020-03-30 23:16:41 +0800

    人员信息界面

--- app/src/main/java/com/txtled/gpa220/user/UserInfoActivity.java
+++ app/src/main/java/com/txtled/gpa220/user/UserInfoActivity.java
@@ -1,14 +1,52 @@
 package com.txtled.gpa220.user;
 
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
 import com.txtled.gpa220.R;
 import com.txtled.gpa220.base.MvpBaseActivity;
 import com.txtled.gpa220.user.mvp.UserContract;
 import com.txtled.gpa220.user.mvp.UserPresenter;
+import com.txtled.gpa220.widget.CustomTextView;
+
+import butterknife.BindView;
 
 /**
  * Created by Mr.Quan on 2020/3/30.
  */
 public class UserInfoActivity extends MvpBaseActivity<UserPresenter> implements UserContract.View {
+    @BindView(R.id.img_measure)
+    ImageView imgMeasure;
+    @BindView(R.id.ctv_measure)
+    CustomTextView ctvMeasure;
+    @BindView(R.id.rl_measure)
+    LinearLayout rlMeasure;
+    @BindView(R.id.img_change)
+    ImageView imgChange;
+    @BindView(R.id.ctv_change)
+    CustomTextView ctvChange;
+    @BindView(R.id.rl_change)
+    LinearLayout rlChange;
+    @BindView(R.id.img_export)
+    ImageView imgExport;
+    @BindView(R.id.ctv_export)
+    CustomTextView ctvExport;
+    @BindView(R.id.rl_export)
+    LinearLayout rlExport;
+    @BindView(R.id.img_delete)
+    ImageView imgDelete;
+    @BindView(R.id.ctv_delete)
+    CustomTextView ctvDelete;
+    @BindView(R.id.rl_delete)
+    LinearLayout rlDelete;
+    @BindView(R.id.rl_user_list)
+    RelativeLayout rlUserList;
+    @BindView(R.id.img_user)
+    ImageView imgUser;
+    @BindView(R.id.ctv_user_name)
+    CustomTextView ctvUserName;
+
     @Override
     public void setInject() {
         getActivityComponent().inject(this);
@@ -19,6 +57,7 @@ public class UserInfoActivity extends MvpBaseActivity<UserPresenter> implements 
         initToolbar();
         setNavigationIcon(true);
         tvTitle.setText(R.string.user_info);
+
 
     }
 
