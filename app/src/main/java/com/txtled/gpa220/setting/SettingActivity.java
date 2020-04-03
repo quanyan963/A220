654ad56961610d3b16a39cad4d4263bb262935d8
quanyan963 <709641706@qq.com>
2020-04-03 17:23:08 +0800

    增加版本号

--- app/src/main/java/com/txtled/gpa220/setting/SettingActivity.java
+++ app/src/main/java/com/txtled/gpa220/setting/SettingActivity.java
@@ -1,13 +1,24 @@
 package com.txtled.gpa220.setting;
 
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.txtled.gpa220.R;
 import com.txtled.gpa220.base.MvpBaseActivity;
 import com.txtled.gpa220.setting.mvp.SetContract;
 import com.txtled.gpa220.setting.mvp.SetPresenter;
+import com.txtled.gpa220.widget.CustomTextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Mr.Quan on 2020/4/3.
  */
 public class SettingActivity extends MvpBaseActivity<SetPresenter> implements SetContract.View {
+    @BindView(R.id.ctv_setting_version)
+    CustomTextView ctvSettingVersion;
+
     @Override
     public void setInject() {
         getActivityComponent().inject(this);
@@ -15,12 +26,18 @@ public class SettingActivity extends MvpBaseActivity<SetPresenter> implements Se
 
     @Override
     public void init() {
-
+        initToolbar();
+        tvTitle.setText(R.string.setting);
+        try {
+            ctvSettingVersion.setText(getPackageManager().getPackageInfo(getPackageName(),0).versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public int getLayout() {
-        return 0;
+        return R.layout.activity_setting;
     }
 
     @Override
